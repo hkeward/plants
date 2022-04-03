9b847ec477b595048c046151e09d67e3498f2eb3
Heather Ward <heather@dnastack.com>
2022-04-03 13:58:08 -0400

    Add basic markdown parsing

---
+++ src/views/Plant.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Lobelia class="plant-markdown" />
+  </div>
+</template>
+
+<script>
+import Lobelia from "../assets/plants/Lobelia.md";
+
+export default {
+  name: "Plant",
+  components: { Lobelia },
+};
+</script>
+
+<style>
+.plant-markdown {
+  text-align: left;
+}
+</style>
